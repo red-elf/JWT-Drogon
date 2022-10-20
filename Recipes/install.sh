501081db7fe6de3796e4367ccc2b92aff638455e
Milos Vasic <i@mvasic.ru>
2022-10-20 16:08:05 +0300

    The installation recipe.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Drogon JWT plugin is ready for use"
+HERE="$(pwd)"
+VERSIONABLE_BUILD_SCRIPT="$HERE/Versionable/versionable_build.sh"
+VERSIONABLE_INSTALL_SCRIPT="$HERE/Versionable/versionable_install.sh"
+
+if ! test -e "$VERSIONABLE_BUILD_SCRIPT"; then
+
+  echo "ERROR: The versionable build script not found at expected location: '$VERSIONABLE_BUILD_SCRIPT'"
+  exit 1
+fi
+
+if ! test -e "$VERSIONABLE_INSTALL_SCRIPT"; then
+
+  echo "ERROR: The versionable install script not found at expected location: '$VERSIONABLE_INSTALL_SCRIPT'"
+  exit 1
+fi
+
+cd "$HERE" && sh "$VERSIONABLE_BUILD_SCRIPT" Library .. &&  sh "$VERSIONABLE_INSTALL_SCRIPT" Library
