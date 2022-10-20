8c28dc05780099bbb74cbe8c680923634d254fa2
Milos Vasic <i@mvasic.ru>
2022-10-20 16:16:06 +0300

    The install recipe.

--- Recipes/install.sh
+++ Recipes/install.sh
@@ -16,4 +16,27 @@ if ! test -e "$VERSIONABLE_INSTALL_SCRIPT"; then
   exit 1
 fi
 
-cd "$HERE" && sh "$VERSIONABLE_BUILD_SCRIPT" Library .. &&  sh "$VERSIONABLE_INSTALL_SCRIPT" Library
+cd "$HERE" && echo "Preparing the dependency linking"
+
+if ! test -e "$HERE/Library/json";
+
+  if ! ln -s "$HERE/_Dependencies/Cache/JSON/Library" "$HERE/Library/json"; then
+
+    echo "ERROR: Could not link the 'json' dependency"
+    exit 1
+  fi
+then
+
+if ! test -e "$HERE/Library/cpp-jwt";
+
+  if ! ln -s "$HERE/_Dependencies/Cache/CPP-JWT/Library" "$HERE/Library/cpp-jwt"; then
+
+    echo "ERROR: Could not link the 'cpp-jwt' dependency"
+    exit 1
+  fi
+then
+
+sh "$VERSIONABLE_BUILD_SCRIPT" Library ..
+
+# TODO:
+# &&  sh "$VERSIONABLE_INSTALL_SCRIPT" Library
