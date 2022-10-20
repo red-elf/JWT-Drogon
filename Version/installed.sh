181be6bb68e79630e386f3a6d4169f8cbfb1e9ce
Milos Vasic <i@mvasic.ru>
2022-10-20 16:30:59 +0300

    The 'installed' recipe updated.

--- Version/installed.sh
+++ Version/installed.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-HERE="$(pwd)"
-CURRENT_SCRIPT="$HERE/Versionable/current.sh"
-
-if ! test -e "$CURRENT_SCRIPT"; then
-
-  echo "ERROR: The current script was not found at path: '$CURRENT_SCRIPT'"
-  exit 1
-fi
-
-sh "$CURRENT_SCRIPT"
+echo "FORCE_INSTALLATION"
